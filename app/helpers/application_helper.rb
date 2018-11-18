62d847a012b0933633bd35105b611422da204503
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-11-18 12:10:54 +0100

    moving on front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def number_to_currency_br(number)
     number_to_currency(number, :unit => "", :separator => ",", :delimiter => ".")
   end
+
+  def number_to_currency_net(number)
+    number_to_currency(number,precision: 0, :unit => "", :separator => ",", :delimiter => ".")
+  end
 end
--- app/models/user_fiat_account.rb
+++ app/models/user_fiat_account.rb
@@ -1,4 +1,6 @@
 class UserFiatAccount < ApplicationRecord
   belongs_to :user
   has_many :transactions
+  validates :currency_code, inclusion: { in: %w(EUR USD PHP HRK HKD GBP NGN BRL EUR),
+      message: "Tu te crois malin?" }
 end
