b7626529e01e2bfeb67e2e8b63b1e656019260f1
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-11-22 09:11:29 +0100

    trying mymaps rendering

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :transactions, through: :user_fiat_accounts
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
--- app/models/user_fiat_account.rb
+++ app/models/user_fiat_account.rb
@@ -1,6 +1,11 @@
 class UserFiatAccount < ApplicationRecord
   belongs_to :user
   has_many :transactions
+  validate :account_limits
   validates :currency_code, inclusion: { in: %w(EUR USD PHP HRK HKD GBP RUB BRL EUR),
       message: "Tu te crois malin?" }
+
+    def account_limits
+      !UserFiatAccount.where(user: self.user).any?{|e| e.currency_code == self.currency_code}
+    end
 end
